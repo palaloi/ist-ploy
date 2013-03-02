5983080852f618fc85bdc48e90d0989f0795b4c8
Mintra Ruensuk <kikist@gmail.com>
2013-03-02 19:06:33 +0700

    Edit portfolio page

--- ist-ploy/app/controllers/portfolio_controller.rb
+++ ist-ploy/app/controllers/portfolio_controller.rb
@@ -37,29 +37,25 @@ class PortfolioController < ApplicationController
     params[:hidden_tag].split(",").each do |d|
       tag = Tag.find_by_name(d)
       unless tag.nil?
-        puts "Tagggggggg #{tag.id}"
-        puts "Porttttttt #{@portfolio.id}"
         existing_tag = PortfolioTag.find(:all, :conditions => {:tag_id => tag.id, :portfolio_id => @portfolio.id})
-        puts "existing_tag ====> #{existing_tag}"
-        puts "existing_tag.nil? #{existing_tag.nil?}"
-        puts "================= #{d}"
         PortfolioTag.create(:tag => tag, :portfolio => @portfolio) unless existing_tag.any?
       else 
         PortfolioTag.create(:tag => Tag.create(:name => d), :portfolio => @portfolio)
       end
-      
       # @portfolio.tags.push(Tag.find_by_name(d)) unless Tag.find_by_name(d).nil?
     end unless params[:hidden_tag].blank?
     if @portfolio.save
-     redirect_to portfolio_show_path({:portfolio_id => @portfolio.id})
+     redirect_to portfolio_show_path({:portfolio_id => @portfolio.id, :user_id => @user.id})
     else
       render :action => :upload_detail, :params => {:portfolio => @portfolio.id}
     end
   end
 
   def show
-    @user = current_user
+    @user = User.find(params[:user_id])
+    @current_user = current_user
     @portfolio = Portfolio.find(params[:portfolio_id])
+    @portfolio["tags"] = @portfolio.portfolio_tags
     @title =  @portfolio.title.nil?? "Portfolio: " + @user.name: "Portfolio: "+ @portfolio.title
   end
 end
--- ist-ploy/app/helpers/application_helper.rb
+++ ist-ploy/app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+	def request_from
+    params[:controller] + "#" + params[:action]
+    # ""
+  end
 end
