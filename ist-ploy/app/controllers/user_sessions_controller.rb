4200b2de499cc5a6f6867d852cc9ca89fdeb09b5
Mintra Ruensuk <kikist@gmail.com>
2013-03-16 08:39:54 +0700

    Edit top bar

--- ist-ploy/app/controllers/user_sessions_controller.rb
+++ ist-ploy/app/controllers/user_sessions_controller.rb
@@ -3,10 +3,12 @@ class UserSessionsController < ApplicationController
 	before_filter :require_user,  :only => :destroy
 
   def new
+    @titile = "Login"
     @user_session = UserSession.new
   end
 
   def create
+    @titile = "Login"
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
       flash[:notice] = "Login successful!"
